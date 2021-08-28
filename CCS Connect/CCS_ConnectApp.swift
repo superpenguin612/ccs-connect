696b191be2c87211756e98b0435adf0d6238401f
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2021-08-28 11:59:25 -0400

    Initial Commit

---
+++ CCS Connect/CCS_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCS_ConnectApp.swift
+//  CCS Connect
+//
+//  Created by David Racovan on 8/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CCS_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCS Connect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CCS Connect
+//
+//  Created by David Racovan on 8/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
